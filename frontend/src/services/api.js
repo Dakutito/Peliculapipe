a43471ce39a369276a3668635baa23bf85f3a554
Dakutito <122334969+Dakutito@users.noreply.github.com>
2026-01-07 14:39:22 -0500

    Toggle API_URL between environment variable and hardcoded

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = "https://peliculapipe.onrender.com/api";
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://peliculapipe.onrender.com/api";
+//const API_URL = import.meta.env.VITE_API_URL;
 
 // Películas
 export const getTrending = () => axios.get(`${API_URL}/movies/trending`);
